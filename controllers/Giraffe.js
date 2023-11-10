c1e932b340828057c640628700f5df91d0c09d54
Ajit Ubbanapally <ajitpurnahk@gmail.com>
2023-11-10 11:29:41 -0600

    Screen shot 1 of Postman or Browser with URL

--- controllers/Giraffe.js
+++ controllers/Giraffe.js
@@ -1,64 +1,76 @@
 var Giraffe = require('../models/Giraffe');
 // List of all Giraffe
 // List of all Costumes
-exports.Giraffe_list=async function(req, res) {
-try{
-Giraffe = await Giraffe.find();
-res.send(Giraffe);
-}
-catch(err){
-res.status(500);
-res.send(`{"error": ${err}}`);
-}
+exports.Giraffe_list = async function (req, res) {
+  try {
+    Giraffe = await Giraffe.find();
+    res.send(Giraffe);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+  }
 };
 
 
 // Handle Giraffe delete form on DELETE.
-exports.Giraffe_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Giraffe delete DELETE ' + req.params.id);
+exports.Giraffe_delete = function (req, res) {
+  res.send('NOT IMPLEMENTED: Giraffe delete DELETE ' + req.params.id);
 };
 // Handle Giraffe update form on PUT.
-exports.Giraffe_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Giraffe update PUT' + req.params.id);
+exports.Giraffe_update_put = function (req, res) {
+  res.send('NOT IMPLEMENTED: Giraffe update PUT' + req.params.id);
 };
 
 
 // VIEWS
 // Handle a show all view
-exports.Giraffe_view_all_Page = async function(req, res) {
-    try{
+exports.Giraffe_view_all_Page = async function (req, res) {
+  try {
     theGiraffe = await Giraffe.find();
     res.render('Giraffe', { title: 'Giraffe Search Results', results: theGiraffe });
-    }
-    catch(err){
+  }
+  catch (err) {
     res.status(500);
     res.send(`{"error": ${err}}`);
-    }
-    };
+  }
+};
 
 
-    // for a specific Giraffe.
-exports.Giraffe_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Giraffe detail: ' + req.params.id);
-    };
-    // Handle Giraffe create on POST.
-    exports.Giraffe_create_post = async function(req, res) {
-        console.log(req.body)
-        let document = new Giraffe();
-        // We are looking for a body, since POST does not have query parameters.
-        // Even though bodies can be in many different formats, we will be picky
-        // and require that it be a json object
-        // {"Giraffe_type":"goat", "cost":12, "size":"large"}
-        document.Giraffe_breed = req.body.Giraffe_breed;
-        document.Giraffe_age = req.body.Giraffe_age;
-        document.Giraffe_heightfeet = req.body.Giraffe_heightfeet;
-        try{
-        let result = await document.save();
-        res.send(result);
-        }
-        catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-        }
-        
-        };
+// for a specific Giraffe.
+
+exports.Giraffe_detail = async function (req, res) {
+  console.log("detail" + req.params.id)
+  try {
+    result = await Giraffe.findById(req.params.id)
+    res.send(result)
+  } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+  }
+};
+
+
+
+// Handle Giraffe create on POST.
+exports.Giraffe_create_post = async function (req, res) {
+  console.log(req.body)
+  let document = new Giraffe();
+  // We are looking for a body, since POST does not have query parameters.
+  // Even though bodies can be in many different formats, we will be picky
+  // and require that it be a json object
+  // {"Giraffe_type":"goat", "cost":12, "size":"large"}
+  document.Giraffe_breed = req.body.Giraffe_breed;
+  document.Giraffe_age = req.body.Giraffe_age;
+  document.Giraffe_heightfeet = req.body.Giraffe_heightfeet;
+  try {
+    let result = await document.save();
+    res.send(result);
+  }
+  catch (err) {
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+
+  }
+
+};
--- routes/Giraffe.js
+++ routes/Giraffe.js
@@ -3,4 +3,6 @@ const Giraffe_controlers= require('../controllers/Giraffe');
 var router = express.Router();
 /* GET Giraffe */
 router.get('/', Giraffe_controlers.Giraffe_view_all_Page );
+// GET request for one Giraffe.
+//router.get('/Giraffe/:id',Giraffe_controller.Giraffe_detail);
 module.exports = router;

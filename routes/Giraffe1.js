078fe53a1e824a9ac4ee3e7cca63baed4a28b416
Ajit Ubbanapally <ajitpurnahk@gmail.com>
2023-11-29 10:25:45 -0600

    secured

--- routes/Giraffe1.js
+++ routes/Giraffe1.js
@@ -7,6 +7,12 @@ router.get('/', Giraffe_controlers.Giraffe_view_all_Page );
 // GET request for one Giraffe.
 //router.get('/Giraffe/:id',Giraffe_controller.Giraffe_detail);
 
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 
 
 
@@ -16,7 +22,22 @@ router.get('/detail', Giraffe_controlers.Giraffe_view_one_Page);
 /* GET create Giraffe page */
 router.get('/create', Giraffe_controlers.Giraffe_create_Page);
 /* GET create update page */
-router.get('/update', Giraffe_controlers.Giraffe_update_Page);
+//router.get('/update', Giraffe_controlers.Giraffe_update_Page);
+
+router.get('/update',secured, Giraffe_controlers.Giraffe_update_Page);
+// router.post('/login', passport.authenticate('local'), function(req, res) {
+//     res.redirect('/');
+//     });
+// A little function to check if we have an authorized user and continue on
+
+//or redirect to login.
+
+
+
+/* GET update Giraffe page */
+router.get('/update', secured,
+Giraffe_controlers.Giraffe_update_Page);
+
 
 /* GET delete Giraffe page */
 router.get('/delete', Giraffe_controlers.Giraffe_delete_Page);

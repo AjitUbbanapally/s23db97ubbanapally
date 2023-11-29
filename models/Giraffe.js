f2946f7f8f289fd887e48b107ed064635d396b0a
Ajit Ubbanapally <ajitpurnahk@gmail.com>
2023-11-29 10:32:14 -0600

    Min,max

--- models/Giraffe.js
+++ models/Giraffe.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const GiraffeSchema = mongoose.Schema({
 Giraffe_breed: String,
 Giraffe_age: Number,
-Giraffe_heightfeet:Number,
+Giraffe_heightfeet:{ type:Number,
+     min:[10, "height sshould be minimum 10 feet"],
+   max:[50,"Maximum height should be 50 feet"]
+ }
 
 });
 module.exports = mongoose.model("Giraffe",GiraffeSchema)
